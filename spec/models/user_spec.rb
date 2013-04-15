5c3e652d39c9de9f437dd7fa30572bfb835015e6
David Niemi <niemidavidj@gmail.com>
2013-04-14 21:09:08 -0500

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 require 'spec_helper'
 
 describe User do
@@ -29,9 +19,24 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
   it { should_not be_admin }
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: true)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
 
   describe "with admin attribute set to 'true'" do
     before do
@@ -41,8 +46,8 @@ describe User do
 
     it { should be_admin }
   end
-
-describe "when name is not present" do
+  
+  describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
@@ -106,7 +111,7 @@ describe "when name is not present" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-   
+
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
@@ -139,7 +144,7 @@ describe "when name is not present" do
   end
 
 
-describe "micropost associations" do
+  describe "micropost associations" do
 
     before { @user.save }
     let!(:older_micropost) do 
@@ -152,7 +157,7 @@ describe "micropost associations" do
     it "should have the right microposts in the right order" do
       @user.microposts.should == [newer_micropost, older_micropost]
     end
-
+  
     it "should destroy associated microposts" do
       microposts = @user.microposts.dup
       @user.destroy
@@ -162,15 +167,49 @@ describe "micropost associations" do
       end
     end
   
-  describe "status" do
+    describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
-
